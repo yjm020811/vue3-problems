e6f97c7f10b52213e248b1c0e2fbb90fb633584c
yjm020811 <2806654030@qq.com>
2025-01-17 14:57:48 +0800

    add：el-card响应式布局

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,19 @@ import compress from "./components/compress.vue";
 import screencut from "./components/screencut.vue";
 import File from "./components/file.vue";
 import bigFile from "./components/bigFile.vue";
+import flexCards from "./components/flexCards.vue";
 </script>
 
 <template>
   <compress />
-  <br />
+  <hr />
   <screencut />
-  <br />
+  <hr />
   <File />
-  <br />
+  <hr />
   <bigFile />
+  <hr />
+  <flexCards />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/flexCards.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h3>响应式卡片</h3>
+        <div style="width: 96%;margin: 0 auto;">
+            <el-row :gutter="20">
+                <el-col :xs="24" :sm="12" :md="8" :lg="6" v-for="i in 6" :key="i">
+                    <el-card style="max-width: 360px;margin-bottom: 20px;">
+                        <template #header>
+                            <div class="card-header">
+                                <span>Card name</span>
+                            </div>
+                        </template>
+                        <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+                        <template #footer>Footer content</template>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
